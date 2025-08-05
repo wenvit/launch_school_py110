8683dc2a7366b141479984aa86cbc2a92c7a9d74
wenvit <wendyvit@icloud.com>
2025-08-05 12:51:28 -0500

    add another solution using list comprehension

--- small_problems/easy3/double_char1.py
+++ small_problems/easy3/double_char1.py
@@ -4,13 +4,20 @@ Write a function that takes a string, doubles every character in the string,
 then returns the result as a new string.
 
 '''
+# original solution
 
+# def repeater(input_str):
+#     repeated_str = ''
+
+#     for char in input_str:
+#         repeated_str += char * 2
+#     return repeated_str
+
+
+# another way
 def repeater(input_str):
-    repeated_str = ''
 
-    for char in input_str:
-        repeated_str += char * 2
-    return repeated_str
+    return ''.join([char * 2 for char in input_str])
 
 print(repeater('Hello') == "HHeelllloo")              # True
 print(repeater('Good job!') == "GGoooodd  jjoobb!!")  # True
